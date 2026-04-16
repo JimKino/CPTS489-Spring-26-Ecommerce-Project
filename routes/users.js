6c967a8c7d292344306bf105afde8922f356f169
RyanWSU <ryan.blosser@wsu.edu>
2026-04-15 21:26:34 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var sqlite = require('sqlite3').verbose();
+
+const db = new sqlite.Database('./storedb.sqlite');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,5 +18,20 @@ router.get('/login', function(req, res, next) {
 router.get('/register', function(req, res, next) {
   res.render('register');
 });
+
+router.post('/add_user', function(req, res, next) {
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  //The ? symbol is a variable filled out later by the run command
+  //all values must be passed in through the run command
+  db.prepare(`INSERT INTO accounts (actEmail, actPassword, actFname, actLname, actType)
+    VALUES (?, ?, ?, ?, ?)`).run(email, password, fname, lname, "customer");
+  console.log("Added user " + email);
+
+  res.redirect('/users/login')
+})
 
 module.exports = router;
