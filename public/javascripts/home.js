eb80ae7cfcc7845f3dc7e851e4fdd92b2976b4d8
RyanWSU <ryan.blosser@wsu.edu>
2026-04-17 23:46:54 -0700

    Add files via upload

---
+++ public/javascripts/home.js
@@ -0,0 +1,23 @@
+fetch("/homePopulate", { method: "POST", headers: { "Content-Type": "application/json" }})
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    for(let i = 0; i < data.length; i++)
+    {
+        const main = document.getElementById('main');
+        const item = document.createElement('div');
+        item.classList.add('productContainer');
+        item.innerHTML += `
+        <div class="productImage">
+            <img src="/images/${data[i].listImage}">
+        </div>
+        <div class="productDesc">
+            <h1>${data[i].listName}</h1>
+            <h2>$${data[i].listPrice}</h2>
+            <p>${data[i].listDesc}</p>
+        </div>`;
+        main.append(item);
+    }
+})
