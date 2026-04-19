557bf188b2fa1e46d85e7168156fc1b3f1cb33eb
RyanWSU <ryan.blosser@wsu.edu>
2026-04-18 19:27:31 -0700

    Update productPage.js

--- public/javascripts/productPage.js
+++ public/javascripts/productPage.js
@@ -1,29 +1,3 @@
-const url = window.location.href;
-const split_url = url.split('=');
-const id = split_url[1];
-
-const body_object = {
-    id
-};
-
-const body_json = JSON.stringify(body_object);
-
-fetch("/itemPopulate", { method: "POST", body: body_json, headers: { "Content-Type": "application/json" }})
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    const image = document.getElementById('productImage');
-    const desc = document.getElementById('productDesc');
-    const buy = document.getElementById('productBuy');
-    image.innerHTML += `<img src="/images/${data.listImage}">`;
-    desc.innerHTML += `<h1>${data.listName}</h1>
-    <h2>$${data.listPrice}</h2>
-    <p>${data.listDesc}</p>`;
-    buy.innerHTML += `<h2>$${data.listPrice}</h2>
-    <button type="submit" class="btn btn-primary">Add to Cart</button>`;
-})
-
 const review_submit = document.getElementById("review_submit");
 const input_review = document.getElementById("input_review");
 const btnReview = document.getElementById("btnReview");
