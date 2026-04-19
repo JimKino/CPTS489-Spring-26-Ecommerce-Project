4ed541887a12b73fdd4c0d58dabf64382c1eae1d
RyanWSU <ryan.blosser@wsu.edu>
2026-04-19 01:29:40 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -40,23 +40,27 @@ router.get('/profile', function(req, res, next) {
 
 router.post('/createListing', function(req, res, next) {
   var listNo = 1;
-  var listName = req.body.item_name;
-  var listDesc = req.body.item_desc;
-  var listImage = req.body.item_image;
-  var listPrice = req.body.item_price;
-  var listQuanity = req.body.item_quantity;
-  var listSeller = "seller@wsu.edu";
+  var listName = req.body.name;
+  var listDesc = req.body.desc;
+  var listImage = req.body.image;
+  var listPrice = req.body.price;
+  var listQuanity = req.body.quantity;
+  var listSeller = req.body.email;
 
   const db = new DatabaseSync('./storedb.sqlite');
   var row = db.prepare(`SELECT COUNT(*) FROM listings`).get();
   listNo = listNo + row["COUNT(*)"];
 
-  db.prepare(`INSERT INTO listings (listNo, listName, listDesc, listImage, listPrice, listQuanity, listSeller)
+  try{
+  db.prepare(`INSERT INTO listings (listNo, listName, listDesc, listImage, listPrice, listQuantity, listSeller)
   VALUES (?, ?, ?, ?, ?, ?, ?)`).run(listNo, listName, listDesc, listImage, listPrice, listQuanity, listSeller);
   console.log("Added listing " + listName);
-
+  }
+  catch (e) {
+    console.log(e);
+  }
   db.close();
   res.redirect('/');
-})
+});
 
 module.exports = router;
