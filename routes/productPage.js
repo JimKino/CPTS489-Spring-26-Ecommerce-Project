66ae5da17f6a221ea20e0f6c5a04360755092a66
RyanWSU <ryan.blosser@wsu.edu>
2026-04-18 17:39:37 -0700

    Update productPage.js

--- routes/productPage.js
+++ routes/productPage.js
@@ -7,4 +7,11 @@ router.get('/', function(req, res) {
   res.render('productPage');
 });
 
+router.post('/itemPopulate', function(req, res, next) {
+  const db = new DatabaseSync('./storedb.sqlite');
+  var rows = db.prepare(`SELECT * FROM listings WHERE listNo=?`).get(req.body.id);
+  db.close();
+  res.send(rows);
+});
+
 module.exports = router;
