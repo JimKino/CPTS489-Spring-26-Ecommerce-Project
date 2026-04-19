9aa3edb297c2d78367dd7c657e5b2049faa3d79a
RyanWSU <ryan.blosser@wsu.edu>
2026-04-18 23:26:17 -0700

    Update productPage.js

--- routes/productPage.js
+++ routes/productPage.js
@@ -20,4 +20,28 @@ router.post('/itemPopulate', function(req, res, next) {
   res.send(rows);
 });
 
+router.post('/review', function(req, res, next) {
+  const rating = req.body.rating;
+  const title = req.body.title;
+  const desc = req.body.desc;
+  const id = req.body.id;
+  const email = req.body.email;
+
+  const db = new DatabaseSync('./storedb.sqlite');
+  db.prepare(`INSERT INTO reviews (revRating, revTitle, revDesc, revList, revAct)
+    VALUES (?, ?, ?, ?, ?)`).run(rating, title, desc, id, email);
+  console.log("Added review");
+
+  db.close();
+
+  res.redirect(`/productPage/${id}`)
+});
+
+router.post('/getReviews', function(req, res, next) {
+  const db = new DatabaseSync('./storedb.sqlite');
+  var rows = db.prepare(`SELECT * FROM reviews WHERE revList=?`).all(req.body.id);
+  db.close();
+  res.send(rows);
+});
+
 module.exports = router;
